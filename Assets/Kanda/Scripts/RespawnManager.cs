89abbb7c36ffc39b3f85b1007e5640d7e82c8d29
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-09-15 23:25:46 +0900

    [ add ] script追加 シーン遷移後リスポーン

---
+++ Assets/Kanda/Scripts/RespawnManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour
+{
+    [Header("Respawn Settings")]
+    [SerializeField] private Vector3 spawnPosition = new Vector3(0f, 1f, 0f);
+    [SerializeField] private Vector3 spawnRotation = new Vector3(0f, 0f, 0f);
+
+    [Header("Target Objects")] 
+    [SerializeField] private GameObject player;
+    [SerializeField] private Camera playerCamera;
+
+    void Start()
+    {
+        SetPlayerSpawnPosition();
+    }
+
+    private void SetPlayerSpawnPosition()
+    {
+        if (player == null)
+        { 
+            MoveController moveController = FindObjectOfType<MoveController>();
+            if (moveController != null)
+                player = moveController.gameObject;
+        }
+        
+        if (playerCamera == null)
+            playerCamera = Camera.main;
+    
+        if (player != null)
+        {
+            player.transform.position = spawnPosition;
+            player.transform.eulerAngles = spawnRotation;
+            Debug.Log($"プレイヤーをリスポーン地点に配置:{spawnPosition}");
+        }
+        ResetCameraRotation();
+    }
+
+    private void ResetCameraRotation()
+    {
+        if (playerCamera != null)
+        {
+            CameraController cameraController = playerCamera.GetComponent<CameraController>();
+            if (cameraController != null)
+            {
+                cameraController.ResetCameraRotation();
+                Debug.Log("カメラの回転をリセット");
+            }
+            else
+            {
+                playerCamera.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+    public void ManualRespawn()
+    {
+        SetPlayerSpawnPosition();
+    }
+
+    public void RespownAtPosition(Vector3 position, Vector3 rotation)
+    {
+        spawnPosition = position;
+        spawnRotation = rotation;
+        SetPlayerSpawnPosition();
+    }
+}
