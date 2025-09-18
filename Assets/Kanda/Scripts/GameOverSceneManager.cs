34baf408cad7ab0e1c989f3e370961abd14bdafd
vahkd33114 <vahkd33114@yahoo.co.jp>
2025-09-18 11:09:14 +0900

    [ update ] script変更

---
+++ Assets/Kanda/Scripts/GameOverSceneManager.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverSceneManager : MonoBehaviour
+{
+    [SerializeField] private Button retryButton;
+
+    void Start()
+    {
+        if (retryButton != null)
+            retryButton.onClick.AddListener(RetryGame);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            RetryGame();
+    }
+
+    private void RetryGame()
+    {
+        SceneManager.LoadScene("2 Story");
+    }
+}
